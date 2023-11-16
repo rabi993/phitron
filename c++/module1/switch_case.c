d4caebbcb4d0394d1f9973f8afa6245b3df05c2c
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2023-11-16 15:42:02 +0600

    add modules dsa

---
+++ c++/module1/switch_case.c
@@ -0,0 +1,30 @@
+#include <iostream>
+#include<iomanip>
+
+#include<stdio.h>
+using namespace std;
+int main(){
+
+    int a ;
+
+    cin>>a;
+
+    switch(a)
+     {
+
+        case 5:
+        cout<<"five"<<endl;
+        case 10:
+        cout<<"ten"<<endl;
+        case 20:
+        cout<<"twenty"<<endl;
+        case 100:
+        cout<<"hundred"<<endl;
+     }
+
+
+
+
+
+return 0;
+}
---
+++ c++/module_2.5/k_max_min.cpp
@@ -0,0 +1,20 @@
+
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+
+int main(){
+
+
+int a,b,c;
+cin>>a>>b>>c;
+int d=min({a,b,c});
+int e=max({a,b,c});
+
+
+   cout<<d<<" "<<e<<endl;
+
+return 0;
+}
