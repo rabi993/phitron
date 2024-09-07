5a730583dda75c43aa223b0f25d8dafb72af4ab6
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2024-09-07 23:26:48 +0600

    Exam system subject marks gain and fine marks

--- python_OOP/exam.py
+++ python_OOP/exam.py
@@ -1,1 +1,33 @@
 class exam:
+    def __init__(self, marks):
+        self.marks = marks
+        
+
+
+    def get_marks(self):
+        return self.marks
+    
+    def written_marks_gain(self, mark):
+        if mark >= 0:
+            self.marks += mark
+
+    def fine_marks(self, mark):
+        
+            self.marks -= mark
+            # print( f'Here is your mark {self.marks}')
+            # print(f'Your marks after fine_marks: {self.marks}')
+            print(f'Your marks after fine_marks: {self.get_marks()}')
+            return self.marks
+
+        
+physics = exam(10)
+physics.written_marks_gain(60)
+physics.fine_marks(25)
+
+print(physics.get_marks())
+
+
+math = exam(10)
+math.written_marks_gain(50)
+
+print(math.get_marks())
