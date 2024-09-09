d3cdffab8b9700636c9e06e91eec607f89a54b9c
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2024-09-09 15:56:17 +0600

    decorator method . import math import time

---
+++ python_OOP/m07_OOP_more/decorator.py
@@ -0,0 +1,27 @@
+
+import math
+import time
+
+def timer(fun):
+    def inner(*args, **kwargs):
+        print('time started')
+        start = time.time()
+        # print(fun)
+        fun(*args, **kwargs)
+        print('time ended')
+        end = time.time()
+        print(f'total time taken {end - start}')
+    return inner
+
+@timer
+def get_factorial(n):
+    print('factorial starting')
+    result = math.factorial(n)
+    print(f'factorial of {n} is: {result}')
+
+
+get_factorial(n=5000)
+# timer(get_factorial)()
+# get_factorial()
+
+# timer()()
