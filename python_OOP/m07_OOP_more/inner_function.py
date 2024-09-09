3bc849f72674f1516f4af2f3e722ecb115589adc
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2024-09-09 14:37:01 +0600

    static attribute static method, @getter @seter method , inner function use

---
+++ python_OOP/m07_OOP_more/inner_function.py
@@ -0,0 +1,52 @@
+# function is a first class object
+
+def double_decker():
+    print('starting the double decker')
+    def inner_fun():
+        print('inside the inner')
+        return 3000
+    return inner_fun
+
+
+# print(double_decker())
+# print(double_decker()())
+
+
+def do_something(work):
+    print('work started')
+    # print(work)
+    work()
+    print('work end')
+    print(work())
+
+
+# do_something(2)
+# do_something('ami busy')
+
+def coding():
+    print('coding in python')
+
+# do_something(coding)
+
+
+# def sleeping():
+#     print('i am sleeping ')
+
+
+
+
+# do_something(sleeping)
+
+def coding1(fun):
+    print('coding in python')
+    def f():
+        print (fun)
+        return fun*fun
+    return f
+
+
+def sleeping1(a):
+    print('i am sleeping :', a)
+    return a*a
+
+do_something(coding1(sleeping1(3)))
--- python_OOP/m07_OOP_more/override.py
+++ python_OOP/m07_OOP_more/override.py
@@ -24,16 +24,21 @@ class Cricketer(Person):
     def exercise(self):
         print('yes')
 
+
+    # + sign operator overload
     def __add__(self, other):
         return self.age + other.age
-    
+    # * sign operator overload
     def __mul__(self, other):
         return self.weight * other.weight
-
+    # len overload
     def __len__(self):
         return self.height
+    # > sign operator overload
     def __gt__(self, other):
         return self.age > other.age
+    
+
 sakib = Cricketer('sakib', 38, 68, 91, 'BD')
 musi = Cricketer('musi', 36, 60, 65, 'BD')
 # sakib.eat()
