3125f377ccdd04a0cf7546af8ba0829681b6fbbe
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2024-09-23 22:20:07 +0600

    javascript intro added module05

---
+++ sw_developement/w2-Javascript/m05_Javascript_Intro/app.js
@@ -0,0 +1,73 @@
+// var test = 'dfg ghth';
+// var test1 = 52;
+// var test2 = true;
+
+// console.log(test);
+
+// console.log(typeof test);
+// console.log(typeof test1);
+// console.log(typeof test2);
+
+// var obj ={
+//     name: 'gius'
+// };
+// console.log(typeof obj);
+
+// var num ="23.5";
+// var num1 =222;
+// var converted_num = parseFloat(num)
+// var  result = num+ num1;
+// var  result1 = converted_num + num1;
+// var  result2 = parseInt(num) + num1;
+
+// console.log(typeof num);
+// console.log(typeof result);
+// console.log(result);
+// console.log(result1);
+// console.log(result2);
+
+// var status = 'rain';
+
+// if (status =='rain'){
+//     console.log("baire jaoa jabe na ")
+// }
+// else if(status=='gorom'){
+//     console.log("baire jaoa jabe na onek gorom ")
+// }
+// else{
+//     console.log("baire jaoa jabe ")
+// }
+
+// var person ={
+//     name :'rabi',
+//     age: 21,
+//     proffesion: 'job',
+//     friends: [  ,  ,  ,  ,  ,],
+//     // brother:{ []}
+
+// }
+
+// function sum(a, b){
+//     console.log('hello')
+
+//     var result = a + b;
+    
+//     return result;
+// }
+
+// var x=5; y =6;
+// res = sum(x,y)
+// console.log(res)
+
+// const sum2 = (a,b)=> a + b;
+
+// const output2 = sum2(20,30);
+// console.log(output2);
+
+const sum2 = (a,b)=> {
+    const c = a+b;
+    return c;
+}
+
+const output2 = sum2(2,30);
+console.log(output2);
