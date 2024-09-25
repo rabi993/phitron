e52fd67d8dabf2e588c8bc096df386c08eac7b6d
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2024-09-25 17:18:11 +0600

    13-Project-Part-3 added

--- sw_developement/w2-Javascript/m07_API/project.js
+++ sw_developement/w2-Javascript/m07_API/project.js
@@ -13,23 +13,40 @@ const loadAllProduct = ()=>{
 
 const displayProduct =(products)=>{
     const productContainer = document.getElementById("productContainer");
+
     products.forEach(product => {
-        // console.log(product)
+        console.log(product)
         const div = document.createElement("div");
 
+        div.classList.add("card");
+
         div.innerHTML=`
-            <img src=" alt" />
-            <h5></h5>
-            <p></p>
+            <img class ="cardImg" src=${product.image} alt="" />
+            <h5>${product.title.slice(0, 30)}</h5>
+            <h3>${product.price}</h3>
+            <p>${product.description.slice(0,100)}</p>
+            
             <button>Details</button>
-            <button>Add to cart</button>
+            <button onclick="handleAddToCart('${product.title}','${product.price} ')" >Add to cart</button>
         `;
         productContainer.appendChild(div);
     });
 
 }
 
+const handleAddToCart =(name, price)=>{
+    // console.log(name, price)
+    const container = document.getElementById("cartMainContainer");
+    const div = document.createElement("div")
+    div.classList.add("cartInfo");
 
+    div.innerHTML = `
+    <p>${name.slice(0,10)}</p>
+    <h3>${price}</h3>
+
+    `
+    container.appendChild(div);
+}
 
 
 
