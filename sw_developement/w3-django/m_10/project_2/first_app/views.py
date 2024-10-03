4ea544c0077d24edafc29d8ea34083923c506e4b
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2024-10-03 09:45:20 +0600

    Django Template Filtering

--- sw_developement/w3-django/m_10/project_2/first_app/views.py
+++ sw_developement/w3-django/m_10/project_2/first_app/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
-
+import datetime
 # Create your views here.
 
 def home(request):
-    d = {'author': 'Rahim', 'age': 30, 'list':[2,5,6,7,10]}
+    d = {'author': 'Rahim', 'age': 5, 'list':[2,5,6,7,10], 'lst': ['python','django','c++'], 'birthday': datetime.datetime.now() , 'val':' ','courses' : [
+        {
+            'id': 1,
+            'name' : 'python',
+            'fee': 5000
+        },
+        {
+            'id': 2,
+            'name' : 'ccccc',
+            'fee': 3000
+        },
+        {
+            'id': 3,
+            'name' : 'database',
+            'fee': 2000
+        }
+    ]}
     return render(request,"home.html", d)
