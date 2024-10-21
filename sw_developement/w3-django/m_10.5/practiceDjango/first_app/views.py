8b412a3e8524496a353529efde71d8820c170677
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2024-10-21 10:47:45 +0600

    simple page navigation

--- sw_developement/w3-django/m_10.5/practiceDjango/first_app/views.py
+++ sw_developement/w3-django/m_10.5/practiceDjango/first_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def home(request):
+    # return HttpResponse("This is home page")
+    return render(request,'first_app/home.html')
+
+def about(request):
+    return render(request,'first_app/about.html')
+def contact(request):
+    return render(request,'first_app/contact.html')
