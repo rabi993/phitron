f41081fd56c9a065522228897da4f5f59f85758e
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2024-10-23 13:36:28 +0600

    forms-in-django  M_13-2 More about Html Form in Django

---
+++ sw_developement/w4-working-with-models/m13-forms-in-django/Forms_in_Django/firstapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('meals/', views.meals, name='meals'),
+    path('about/ttt/', views.about, name='about'),
+    path('form/html_form/', views.submit_form, name='submit_form'),
+    path('submit_show/', views.submit_show, name='submit_show'),
+]
