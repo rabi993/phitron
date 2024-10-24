54366d521ecb31c44f56b3a61810d73ec125d368
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2024-10-24 22:37:47 +0600

    14-2 Creating a Model in Django

---
+++ sw_developement/w4-working-with-models/m14-models&Modelform-in-django/modelForms_in_Django/firstapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('meals/', views.meals, name='meals'),
+    path('about/ttt/', views.about, name='about'),
+    path('form/html_form/', views.submit_form, name='submit_form'),
+    path('submit_show/', views.submit_show, name='submit_show'),
+    # path('django_form/', views.django_form, name='django_form'),
+    # path('django_form/studentForm/', views.studentForm, name='django_form'),
+    path('django_form/passwordValidation/', views.passwordValidation, name='django_form'),
+    path('django_form/modelForms/', views.model_Forms, name='modelForms'),
+]
---
+++ sw_developement/w4-working-with-models/m14-models&Modelform-in-django/modelForms_in_Django/firstapp/views.py
@@ -0,0 +1,273 @@
+
+from django.shortcuts import render
+from . forms import contactForm, studentData, passwordValidationProject, modelForms
+# Create your views here.
+def index(request):
+    
+    
+    meals = [
+        {
+            "strMeal": "BeaverTails",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/ryppsv1511815505.jpg",
+            "idMeal": "52928"
+        },
+        {
+            "strMeal": "Breakfast Potatoes",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/1550441882.jpg",
+            "idMeal": "52965"
+        },
+        {
+            "strMeal": "Canadian Butter Tarts",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/wpputp1511812960.jpg",
+            "idMeal": "52923"
+        },
+        {
+            "strMeal": "Montreal Smoked Meat",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/uttupv1511815050.jpg",
+            "idMeal": "52927"
+        },
+        {
+            "strMeal": "Nanaimo Bars",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/vwuprt1511813703.jpg",
+            "idMeal": "52924"
+        },
+        {
+            "strMeal": "Pate Chinois",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/yyrrxr1511816289.jpg",
+            "idMeal": "52930"
+        },
+        {
+            "strMeal": "Pouding chomeur",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/yqqqwu1511816912.jpg",
+            "idMeal": "52932"
+        },
+        {
+            "strMeal": "Poutine",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/uuyrrx1487327597.jpg",
+            "idMeal": "52804"
+        },
+        {
+            "strMeal": "Rappie Pie",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/ruwpww1511817242.jpg",
+            "idMeal": "52933"
+        },
+        {
+            "strMeal": "Split Pea Soup",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/xxtsvx1511814083.jpg",
+            "idMeal": "52925"
+        }
+    ]
+
+
+    return render(request, 'index.html', { 'meals': meals})
+
+def meals(request):
+    
+    
+    meals = [
+        {
+            "strMeal": "BeaverTails",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/ryppsv1511815505.jpg",
+            "idMeal": "52928"
+        },
+        {
+            "strMeal": "Breakfast Potatoes",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/1550441882.jpg",
+            "idMeal": "52965"
+        },
+        {
+            "strMeal": "Canadian Butter Tarts",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/wpputp1511812960.jpg",
+            "idMeal": "52923"
+        },
+        {
+            "strMeal": "Montreal Smoked Meat",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/uttupv1511815050.jpg",
+            "idMeal": "52927"
+        },
+        {
+            "strMeal": "Nanaimo Bars",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/vwuprt1511813703.jpg",
+            "idMeal": "52924"
+        },
+        {
+            "strMeal": "Pate Chinois",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/yyrrxr1511816289.jpg",
+            "idMeal": "52930"
+        },
+        {
+            "strMeal": "Pouding chomeur",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/yqqqwu1511816912.jpg",
+            "idMeal": "52932"
+        },
+        {
+            "strMeal": "Poutine",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/uuyrrx1487327597.jpg",
+            "idMeal": "52804"
+        },
+        {
+            "strMeal": "Rappie Pie",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/ruwpww1511817242.jpg",
+            "idMeal": "52933"
+        },
+        {
+            "strMeal": "Split Pea Soup",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/xxtsvx1511814083.jpg",
+            "idMeal": "52925"
+        }
+    ]
+
+
+    return render(request, 'meals.html', { 'meals': meals})
+
+def about(request):
+    
+    
+    meals = [
+        {
+            "strMeal": "BeaverTails",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/ryppsv1511815505.jpg",
+            "idMeal": "52928"
+        },
+        {
+            "strMeal": "Breakfast Potatoes",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/1550441882.jpg",
+            "idMeal": "52965"
+        },
+        {
+            "strMeal": "Canadian Butter Tarts",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/wpputp1511812960.jpg",
+            "idMeal": "52923"
+        },
+        {
+            "strMeal": "Montreal Smoked Meat",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/uttupv1511815050.jpg",
+            "idMeal": "52927"
+        },
+        {
+            "strMeal": "Nanaimo Bars",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/vwuprt1511813703.jpg",
+            "idMeal": "52924"
+        },
+        {
+            "strMeal": "Pate Chinois",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/yyrrxr1511816289.jpg",
+            "idMeal": "52930"
+        },
+        {
+            "strMeal": "Pouding chomeur",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/yqqqwu1511816912.jpg",
+            "idMeal": "52932"
+        },
+        {
+            "strMeal": "Poutine",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/uuyrrx1487327597.jpg",
+            "idMeal": "52804"
+        },
+        {
+            "strMeal": "Rappie Pie",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/ruwpww1511817242.jpg",
+            "idMeal": "52933"
+        },
+        {
+            "strMeal": "Split Pea Soup",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/xxtsvx1511814083.jpg",
+            "idMeal": "52925"
+        }
+    ]
+
+
+    return render(request, 'about.html', { 'meals': meals})
+
+def submit_form(request):
+    # print(request.POST)
+    if request.method == 'POST':
+        name = request.POST.get("username")
+        email = request.POST.get("email")
+        return render(request, 'form.html',{'name': name, 'email': email})
+    else:
+        return render(request, 'form.html',)
+    
+
+def submit_show(request):
+    if request.method == 'POST':
+        # print(request.POST)
+        name = request.POST.get("username")
+        email = request.POST.get("email")
+        select = request.POST.get("select")
+        return render(request, 'submit_show.html',{'name': name, 'email': email, 'select': select})
+    else:
+       return render(request, 'submit_show.html')
+    
+
+# def django_form(request):
+#     form = contactForm(request.POST)
+#     if form.is_valid():
+#         print(form.cleaned_data)
+#     return render(request, 'django_form.html',{'form': form})
+
+# def django_form(request):
+#     if request.method == 'POST':
+#         form = contactForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             file = form.cleaned_data['file']
+#             with open('./firstapp/upload/' + file.name, 'wb+') as destination:
+#                 for chunk in file.chunks():
+#                     destination.write(chunk)
+#             print(form.cleaned_data)
+#             return render(request, 'django_form.html',{'form': form})
+#     else:
+#         form = contactForm()
+
+#     return render(request, 'django_form.html',{'form': form})
+def django_form(request):
+    if request.method == 'POST':
+        form = contactForm(request.POST, request.FILES)
+        if form.is_valid():
+            # file = form.cleaned_data['file']
+            # with open('./firstapp/upload/' + file.name, 'wb+') as destination:
+            #     for chunk in file.chunks():
+            #         destination.write(chunk)
+            print(form.cleaned_data)
+    else:
+        form = contactForm()
+
+    return render(request, 'django_form.html',{'form': form})
+
+
+def studentForm(request):
+    if request.method == 'POST':
+        form = studentData(request.POST, request.FILES)
+        if form.is_valid():
+            
+            print(form.cleaned_data)
+    else:
+        form = studentData()
+
+    return render(request, 'django_form.html',{'form': form})
+
+    
+def passwordValidation(request):
+    if request.method == 'POST':
+        form = passwordValidationProject(request.POST)
+        if form.is_valid():
+            
+            print(form.cleaned_data)
+    else:
+        form = passwordValidationProject()
+
+    return render(request, 'django_form.html',{'form': form})
+
+    
+def model_Forms(request):
+    if request.method == 'POST':
+        form = modelForms(request.POST)
+        if form.is_valid():
+            
+            print(form.cleaned_data)
+    else:
+        form = modelForms()
+
+    return render(request, 'modelForms.html',{'form': form})
+
+    
