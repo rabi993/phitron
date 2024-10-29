c19f4e91341179d74630126954422788b70a0b0e
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2024-10-29 14:02:14 +0600

    15-9 Edit Post15-10 Delete Post ;

--- sw_developement/w4-working-with-models/m15-simple-blog-project-part1/blog_project/posts/views.py
+++ sw_developement/w4-working-with-models/m15-simple-blog-project-part1/blog_project/posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from . import forms
+from . import models
 # Create your views here.
 def add_post(request):
     if request.method =='POST':
@@ -11,3 +12,23 @@ def add_post(request):
         post_form = forms.PostForm()
 
     return render(request, 'add_post.html', {'form': post_form})
+
+
+def edit_post(request, id):
+    post = models.Post.objects.get(pk=id)
+    post_form = forms.PostForm(instance=post)
+    # print(post.title)
+    if request.method =='POST':
+        post_form = forms.PostForm(request.POST, instance=post)
+        if post_form.is_valid():
+            post_form.save()
+            return redirect('homepage')
+    # else:
+    #     post_form = forms.PostForm()
+
+    return render(request, 'add_post.html', {'form': post_form})
+
+def delete_post(request, id):
+    post = models.Post.objects.get(pk=id)
+    post.delete()
+    return redirect('homepage')
