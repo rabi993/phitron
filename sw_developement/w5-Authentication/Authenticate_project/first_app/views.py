ecdce6bb836278e43f0993856193343c7e196de4
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2024-11-03 01:07:15 +0600

    17-4 Create user from frontend part-2

--- sw_developement/w5-Authentication/Authenticate_project/first_app/views.py
+++ sw_developement/w5-Authentication/Authenticate_project/first_app/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from . forms import RegisterForm
+from django.contrib import messages
 # Create your views here.
 
 def home(request):
     if request.method =='POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
-            form.save(commit= False)
+            messages.success(request, 'Account created successfully')
+            form.save()
+            print(form.cleaned_data)
     else:
         form = RegisterForm()
     
