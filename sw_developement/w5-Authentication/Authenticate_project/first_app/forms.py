3e6c17a537fe12c892fbc8007d530731d97ea219
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2024-11-03 13:08:40 +0600

    17-8 Change user Data using Builtin Django Forms

--- sw_developement/w5-Authentication/Authenticate_project/first_app/forms.py
+++ sw_developement/w5-Authentication/Authenticate_project/first_app/forms.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django import forms
 
 class RegisterForm(UserCreationForm):
@@ -11,3 +11,8 @@ class RegisterForm(UserCreationForm):
         # fields = '__all__'
         fields = ['username','first_name','last_name','email']
 
+class ChangeUserData(UserChangeForm):
+    password = None
+    class Meta:
+        model = User
+        fields = ['username','first_name','last_name','email']
