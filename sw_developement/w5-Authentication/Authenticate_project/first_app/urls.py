94d45ab9759390ec147b34283b47c601a04fa5c8
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2024-11-03 12:12:28 +0600

    17-7 Change Password using and without using old password

--- sw_developement/w5-Authentication/Authenticate_project/first_app/urls.py
+++ sw_developement/w5-Authentication/Authenticate_project/first_app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('profile/', views.profile, name='profile'),
     path('logout/', views.user_logout, name='logout'),
+    path('pass_change/', views.pass_change, name='passchange'),
+    path('pass_change2/', views.pass_change2, name='passchange2'),
 ]
--- sw_developement/w5-Authentication/Authenticate_project/first_app/views.py
+++ sw_developement/w5-Authentication/Authenticate_project/first_app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from . forms import RegisterForm
 from django.contrib import messages
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm,SetPasswordForm
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 # Create your views here.
 
 def signup(request):
@@ -51,3 +51,31 @@ def profile(request):
 def user_logout(request):
     logout(request)
     return redirect('login')
+
+def pass_change(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            form = PasswordChangeForm(user= request.user, data= request.POST)
+            if form.is_valid():
+                form.save()
+                update_session_auth_hash(request, request.user)
+                return redirect("profile")
+        else:
+            form = PasswordChangeForm(user =request.user)
+        return render(request, './passchange.html', {'form': form})
+    else:
+        return redirect('login')
+
+def pass_change2(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            form = SetPasswordForm(user= request.user, data= request.POST)
+            if form.is_valid():
+                form.save()
+                update_session_auth_hash(request, request.user)
+                return redirect("profile")
+        else:
+            form = SetPasswordForm(user =request.user)
+        return render(request, './passchange.html', {'form': form})
+    else:
+        return redirect('login')
