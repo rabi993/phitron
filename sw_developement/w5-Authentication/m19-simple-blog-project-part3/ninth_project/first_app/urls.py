0ed36db59e070808897d066206dc69dcdfd212d8
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2024-11-10 12:14:10 +0600

    19-9 Working with Session Framework

--- sw_developement/w5-Authentication/m19-simple-blog-project-part3/ninth_project/first_app/urls.py
+++ sw_developement/w5-Authentication/m19-simple-blog-project-part3/ninth_project/first_app/urls.py
@@ -4,7 +4,10 @@ from . import views
 
 urlpatterns = [
     
-    path('', views.home),
-    path('get/', views.get_cookie, name="cookie"),
-    path('del/', views.delete_cookie, name="del"),
+    # path('', views.home),
+    path('', views.set_session),
+    path('get/cookie/', views.get_cookie, name="cookie"),
+    path('get/session/', views.get_session, name="session"),
+    path('del/cookie/', views.delete_cookie, name="del"),
+    path('del/session/', views.delete_session, name="del_session"),
 ]
--- sw_developement/w5-Authentication/m19-simple-blog-project-part3/ninth_project/first_app/views.py
+++ sw_developement/w5-Authentication/m19-simple-blog-project-part3/ninth_project/first_app/views.py
@@ -18,3 +18,29 @@ def delete_cookie(request):
     response = render(request, 'del.html')
     response.delete_cookie('name')
     return response
+
+
+def set_session(request):
+    data ={
+        'name': 'Rahim',
+        'age': 23,
+         'language' : 'Bangla'
+
+    }
+    print(request.session.get_session_cookie_age())
+    print(request.session.get_expiry_date())
+    request.session.update(data)
+    return render(request,'home.html')
+
+def get_session(request):
+    name = request.session.get('name')
+    age = request.session.get('age')
+    print(name)
+    return render(request,'get_session.html', {'name': name,'age': age})
+
+def delete_session(request):
+    # del request.session['name']
+    # request.session.clear()
+    request.session.flush()
+    request.session.clear_expired()
+    return render(request, 'del_session.html')
