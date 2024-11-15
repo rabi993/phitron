a7ee0e8873d6896bac152fc7e1aca95115ce369e
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2024-11-15 12:36:35 +0600

    21-11 Handling Profile Page

--- sw_developement/w6-bank-management-project/mamar_bank/accounts/urls.py
+++ sw_developement/w6-bank-management-project/mamar_bank/accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
-from .views import UserRegistrationView, UserLoginView, UserLogoutView
+from .views import UserRegistrationView, UserLoginView, UserLogoutView, UserBankAccountUpdateView
+# from django.contrib.auth.views import LogoutView
 urlpatterns = [
     
     path('register/', UserRegistrationView.as_view(), name='register' ),
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
+    
+    path('profile/', UserBankAccountUpdateView.as_view(), name='profile' )
 
 ]
